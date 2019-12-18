087acb30a7704e654b15f36898aa98b8cc3a20f0
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-18 23:37:54 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,7 +9,5 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
- 
-  groceries.keys #code your solution here!
+  groceries.keys.flatten #code your solution here!
 end
